4e82e7e6ed69105232c8855c84a345bab9092b95
Sean Gardner <s.gardner94@yahoo.com>
2020-04-25 16:23:28 -0400

    reupload

---
+++ script.js
@@ -0,0 +1,32 @@
+var getLength = prompt("Choose a length between 8-128 characters: ");
+var isCharTypeNum = confirm("Include Numbers? ");
+var isCharTypeUpper = confirm("Include Upper Case Letters? ");
+var isCharTypeLower = confirm("Include Lower Case Letters? ");
+var isCharTypeSpecial = confirm("Include Special Characters? ");
+var newPassword = [];
+var displayPasswordEl = document.querySelector("#displayPassword");
+
+const typeLowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const typeUpperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const typeNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const typeSpecialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', ':', '<', '>', '?'];
+
+var choicesArray = [];
+if (isCharTypeUpper)
+    choicesArray += typeUpperCaseLetters;
+if (isCharTypeLower)
+    choicesArray += typeLowerCaseLetters;
+if (isCharTypeNum)
+    choicesArray += typeNumbers;
+if (isCharTypeSpecial)
+    choicesArray += typeSpecialCharacters;
+
+if (choicesArray.length < typeNumbers.length) {
+    alert("You must choose at least 1 character type.");
+    
+} else {
+    for (let i = 0; i < getLength; i++)
+    newPassword += choicesArray[(Math.floor(Math.random() * (choicesArray.length - 1)))];
+    var convertPasswordToString = (JSON.stringify(newPassword));
+    displayPasswordEl.textContent = convertPasswordToString;
+};
